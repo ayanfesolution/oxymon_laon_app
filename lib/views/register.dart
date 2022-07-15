c73d800b133a970be4e6bab3cfe826b0fc4db1e8
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-07-15 22:57:06 +0100

    complete the register page

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:oxymon_laon_app/utilities/constants.dart';
+import 'package:oxymon_laon_app/utilities/reusablecustombuttons.dart';
+import 'package:oxymon_laon_app/views/signin.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -9,6 +12,7 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  bool isHidden = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +38,121 @@ class _RegisterPageState extends State<RegisterPage> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: const Text('Welcome!!! Create Your Account', style: oMainTextBigBoldTextStyle,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(),
+                  ),
+                  hintText: 'Email',
+                  labelText: 'Email'
+              ),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: TextField(
+                obscureText: isHidden,
+                decoration: InputDecoration(
+                    suffix: GestureDetector(
+                        onTap: () async {
+                          setState((){
+                            if (isHidden) {
+                              isHidden = false;
+                            } else {
+                              isHidden = true;
+                            }
+                          });
+                        },
+                        child: Icon((isHidden == false? Icons.remove_red_eye:CupertinoIcons.eye_slash_fill), size: 20,)),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(),
+                    ),
+                    hintText: 'Password',
+                    labelText: 'Password'
+                ),
+              ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: TextField(
+                obscureText: isHidden,
+                decoration: InputDecoration(
+                    suffix: GestureDetector(
+                        onTap: () async {
+                          setState((){
+                            if (isHidden) {
+                              isHidden = false;
+                            } else {
+                              isHidden = true;
+                            }
+                          });
+                        },
+                        child: Icon((isHidden == false? Icons.remove_red_eye:CupertinoIcons.eye_slash_fill), size: 20,)),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(),
+                    ),
+                    hintText: 'Confirm Password',
+                    labelText: 'Confirm Password'
+                ),
+              ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(),
+                    ),
+                    hintText: 'Firstname',
+                    labelText: 'Firstname'
+                ),
+              ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(),
+                    ),
+                    hintText: 'Lastname',
+                    labelText: 'Lastname'
+                ),
+              ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(),
+                    ),
+                    hintText: 'Phone Number',
+                    labelText: 'Phone Number'
+                ),
+              ),
+          ),
 
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.only(left: 26, right: 26),
+            child: OCustomPassingButton(
+                title: 'Login',
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const SignInPage()));
+                },
+                size: const Size(double.maxFinite, 60)
+            ),
           ),
         ],
       ),
